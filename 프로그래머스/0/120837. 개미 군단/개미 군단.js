b173f1df560b404b47171c12d8c8049263c24c49
Hyotaccato <165138627+Hyotaccato@users.noreply.github.com>
2024-10-05 21:24:14 +0900

    [level 0] Title: 개미 군단, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,7 +1,4 @@
 function solution(hp) {
-    const first = Math.floor(hp / 5);
-    const second = Math.floor((hp - first * 5) / 3);
-    const third = hp - first * 5 - second * 3;
-    const answer = first+second+third;
+    var answer = 0;
     return answer;
 }
