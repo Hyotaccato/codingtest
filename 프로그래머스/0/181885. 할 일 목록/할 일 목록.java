8ac5eae80085cff0c768aabe344c9ac8d9a0477e
Hyotaccato <165138627+Hyotaccato@users.noreply.github.com>
2024-10-08 15:16:57 +0900

    [level 0] Title: 할 일 목록, Time: 0.54 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        String[] answer = {};
+        ArrayList<String> temp = new ArrayList<>();
+        for(int i=0;i<todo_list.length;i++){
+            if(finished[i]==false){
+                temp.add(todo_list[i]);
+            }
+        }
+        answer = new String[temp.size()];
+        for(int i=0;i<temp.size();i++){
+            answer[i]=temp.get(i);
+        }
+        return answer;
+    }
+}
