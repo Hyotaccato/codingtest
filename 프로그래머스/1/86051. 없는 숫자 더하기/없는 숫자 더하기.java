edc05c0fed36d3b5e531dd7abf8c5fcc3fb16bbf
Hyotaccato <165138627+Hyotaccato@users.noreply.github.com>
2024-10-25 18:54:08 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int[] ans = new int[10];
+        
+        for(int i=0; i<numbers.length; i++) {
+            ans[numbers[i]] = 1;
+        }
+
+        for(int i=0; i<ans.length; i++) {
+            if(ans[i] == 0) answer += i;
+        }
+        return answer;
+    }
+}
