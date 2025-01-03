bb0a19cb9ac4bb883e6616a7112c59322de0ed2d
Hyotaccato <165138627+Hyotaccato@users.noreply.github.com>
2025-01-03 22:52:08 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 196 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner 객체 생성
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int arr[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+        for (int i = 0; i < M; i++) { // M개의 줄
+            int left = sc.nextInt() - 1; // 배열 0부터 시작
+            int right = sc.nextInt() - 1;
+
+            while (left < right) {
+                int temp = arr[left];
+                arr[left++] = arr[right];
+                arr[right--] = temp;
+            }
+        }
+        for (int answer : arr)
+            System.out.print(answer + " ");
+    }
+}
