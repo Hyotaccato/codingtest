507da2d31d985e23662e03882b7f86608c3a145a
Hyotaccato <165138627+Hyotaccato@users.noreply.github.com>
2025-04-01 23:43:44 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.52 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
-
 class Solution {
     public int solution(int[] array) {
+        
         Arrays.sort(array);
-        int answer = array[array.length / 2];
+        int answer = array[array.length/2];
+        
         return answer;
     }
 }
