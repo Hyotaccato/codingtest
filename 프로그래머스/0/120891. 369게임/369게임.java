c60ce06c63a1a5372b91b0ad24555bcf1c3ecb87
Hyotaccato <165138627+Hyotaccato@users.noreply.github.com>
2025-04-03 23:42:56 +0900

    [level 0] Title: 369게임, Time: 0.17 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        
-        String s = Integer.toString(order);
-        String [] or = s.split("");
-        
-        for(int i=0; i<or.length; i++){
-            if(or[i].equals("3") ||or[i].equals("6") ||or[i].equals("9")){
+        String order_s=Integer.toString(order);
+        String arr[]=order_s.split("");
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].equals("3") || arr[i].equals("6") || arr[i].equals("9")){
                 answer++;
             }
         }
