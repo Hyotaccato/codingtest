1dfa7507b8c31f330d6e82ccd880030cd6f4de23
Hyotaccato <165138627+Hyotaccato@users.noreply.github.com>
2025-04-09 23:17:47 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,8 +1,6 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int [numbers.length];
-        answer = Arrays.copyOfRange(numbers,num1,num2+1);
-        return answer;
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
