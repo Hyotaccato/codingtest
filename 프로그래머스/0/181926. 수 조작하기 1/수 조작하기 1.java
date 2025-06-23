78e2d54666cb8160746173952c30ee62bf253e1d
Hyotaccato <165138627+Hyotaccato@users.noreply.github.com>
2025-06-23 21:21:06 +0900

    [level 0] Title: 수 조작하기 1, Time: 63.62 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,14 +1,22 @@
 class Solution {
     public int solution(int n, String control) {
-        int answer = 0;
-        for(int i=0; i<control.length(); i++){
-        switch(control.charAt(i)){
-            case 'w' : n+=1; break;
-            case 's' : n-=1; break;
-            case 'd' : n+=10; break;
-            case 'a' : n-=10; break;
+        int answer = n;
+        
+        String[] controlList = control.split("");
+        for(int i=0; i<controlList.length; i++){
+            if(controlList[i].equals("w")){
+                answer += 1;
+            }
+            else if(controlList[i].equals("s")){
+                answer -= 1;
+            }
+            else if(controlList[i].equals("d")){
+                answer += 10;
+            }
+            else if(controlList[i].equals("a")){
+                answer -= 10;
+            }
         }
-        }
-        return n;
+        return answer;
     }
 }
